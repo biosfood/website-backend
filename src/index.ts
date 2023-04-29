dfd74fe3a9034fa068844388209633317f2c33e9
lukas <mail@lukas-eisenhauer.de>
2023-04-29 19:29:31 +0200

    add /resource route to return raw image data for embedding in pages

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { resolvers } from './resolvers'
 import { config } from 'dotenv'
 import express, { Express } from 'express';
+import { Resource } from './database'
 
 config()
 
@@ -48,5 +49,24 @@ const server = new ApolloServer({
 await server.start()
 const app = express();
 app.use(express.json({ limit: '50mb' }))
+app.get('/resource', async (request, response) => {
+  if (request.query.id == undefined) {
+    response.send("error")
+    return
+  }
+  const resource = await Resource.findOne({where: {id: request.query.id}})
+  if (resource == undefined) {
+    response.send("error")
+    return
+  }
+  const base64 = resource.content.substring(resource.content.search(","), resource.content.length)
+  var img = Buffer.from(base64, 'base64');
+  response.writeHead(200, {
+   'Content-Type': 'image',
+   'Content-Length': img.length
+  });
+  response.end(img); 
+})
+
 server.applyMiddleware({app, cors: {origin: "*"}})
 app.listen(4000)
